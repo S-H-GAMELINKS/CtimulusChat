c516d2d6da0aca694bfeb8041f64811934471bd3
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-21 15:43:26 +0900

    add /api/talks

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <vector>
 
 const std::string load_assets(const std::string& path) {
 
@@ -23,12 +24,22 @@ int main() {
 
     httplib::Server svr;
 
+    std::vector<std::string> talks;
+
     std::string body = load_assets("./assets/index.html");
 
     std::string indexjs = load_assets("./assets/index.js");
 
     svr.Get("/", [&](const httplib::Request& req, httplib::Response& res) {
         res.set_content(body, "text/html");
+    });
+
+    svr.Get("/api/talks", [&](const httplib::Request& req, httplib::Pesponse& res) {
+        res.set_content("test", "text/json");
+    });
+
+    svr.Post("/api/talks", [&](const httplib::Request& req, httplib::Response& res) {
+        res.set_content(req.body, "text/plain");
     });
 
     svr.Get("/index.js", [&](const httplib::Request& req, httplib::Response& res) {
